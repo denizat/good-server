9478686585aa98d7ab28ebd173329216bd885c73
denizat <deniza.telci@gmail.com>
2021-06-01 21:48:04 -0500

    Getting better

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ const path = require("path");
 
 const PORT = 8081;
 
+const mimeTypes = {
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+  ".json": "application/json",
+  ".png": "image/png",
+  ".jpg": "image/jpg",
+  ".gif": "image/gif",
+  ".svg": "image/svg+xml",
+  ".wav": "audio/wav",
+  ".opus": "audio/ogg",
+  ".m4a": "audio/mp4",
+  ".mp4": "video/mp4",
+  ".woff": "application/font-woff",
+  ".ttf": "application/font-ttf",
+  ".eot": "application/vnd.ms-fontobject",
+  ".otf": "application/font-otf",
+  ".wasm": "application/wasm",
+};
+
 let folder;
 if (process.argv[2]) {
   folder = process.argv[2];
@@ -12,13 +32,23 @@ if (process.argv[2]) {
 }
 
 const server = http.createServer((req, res) => {
-  if (req.url !== "/favicon.ico") {
-    let dir = folder + req.url;
-    console.log(`request: ${dir}`);
+  let dir = folder + req.url.replaceAll("%20", " ");
+  console.log(`request: ${dir}`);
 
-    fs.readdir(dir, (err, files) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(files.toString(), "utf-8");
+  if (fs.existsSync(dir)) {
+    fs.lstat(dir, (err, stats) => {
+      if (stats.isDirectory()) {
+        fs.readdir(dir, (err, files) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(files.toString(), "utf-8");
+        });
+      } else if (stats.isFile()) {
+        let extname = String(path.extname(dir)).toLowerCase();
+        res.writeHead(200, { "Content-Type": extname });
+        fs.readFile(dir, (err, content) => {
+          res.end(content, "utf-8");
+        });
+      }
     });
   }
 
